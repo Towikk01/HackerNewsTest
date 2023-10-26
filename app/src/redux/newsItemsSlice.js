ec4f4f111ec3a443ae95f6ec257b745471b1942e
t0wikk <anton.masuytkin@gmail.com>
2023-10-26 12:56:36 +0300

    ' '

--- app/src/redux/newsItemsSlice.js
+++ app/src/redux/newsItemsSlice.js
@@ -67,13 +67,13 @@ const fetchSubcommentsByCommentId = createAsyncThunk('stories/fetchSubcommentsBy
       const subcomments = await Promise.all(commentPromises);
       return subcomments;
     }
-
     return [];
   } catch (error) {
     console.error(`Error fetching subcomments for comment ID ${commentId}:`, error);
     throw error;
   }
 });
+
 
 
 const storySlice = createSlice({
